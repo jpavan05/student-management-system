a569e7549e672e0b17c019774adaf224cf52bccb
jpavan05 <jpavan0606@gmail.com>
2025-12-09 15:12:13 +0530

    Add PRN & percentage fields and switch to external CSS

--- students/models.py
+++ students/models.py
@@ -4,10 +4,14 @@ from django.db import models
 from django.db import models
 
 class Student(models.Model):
+
+    prn = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    email = models.EmailField()
     age = models.IntegerField()
     course = models.CharField(max_length=50)
+    percentage = models.FloatField()   # NEW FIELD
+
 
     def __str__(self):
         return self.name
--- students/views.py
+++ students/views.py
@@ -10,21 +10,44 @@ def student_list(request):
 
 def student_create(request):
     if request.method == 'POST':
+        prn = request.POST['prn']
         name = request.POST['name']
         email = request.POST['email']
         age = request.POST['age']
         course = request.POST['course']
-        Student.objects.create(name=name, email=email, age=age, course=course)
+        percentage = request.POST['percentage']
+
+
+        # Check if email already exists
+        if Student.objects.filter(email=email).exists():
+            return render(request, 'students/students_form.html', {
+                'error': 'This email is already registered!',
+                'student': {
+                    'prn':prn,
+                    'name': name,
+                    'email': email,
+                    'age': age,
+                    'course': course,
+                    'percentage':percentage
+                }
+            })
+
+        Student.objects.create(prn=prn, name=name, email=email, age=age, course=course, percentage=percentage)
         return redirect('student_list')
+
     return render(request, 'students/students_form.html')
+
 
 def student_update(request, id):
     student = Student.objects.get(id=id)
     if request.method == 'POST':
+        student.prn = request.POST['prn']
         student.name = request.POST['name']
         student.email = request.POST['email']
         student.age = request.POST['age']
         student.course = request.POST['course']
+        student.percentage = request.POST['percentage']
+
         student.save()
         return redirect('student_list')
     return render(request, 'students/students_form.html', {'student': student})
